090a9568cd42504f695cb64bb918b68ff076455d
olaniyi <olaniyikolawole744@gmail.com>
2022-04-12 14:19:59 -0400

    updated

---
+++ backend.tf
@@ -0,0 +1,9 @@
+#Backend for vpc statefile 
+/*terraform {
+  backend "s3" {
+    bucket = "lennips-bucket"
+    key    = "training/vpc/terraform.tfstates"
+    region = "us-east-1"
+    profile = "default"
+  }
+}*/
