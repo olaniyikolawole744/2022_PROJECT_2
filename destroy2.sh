424fbaf306e029d31a55b5ce469c1fe8ee1d7703
Admin <admin@olawunmi.local>
2022-04-25 10:59:10 -0400

    build infrsatructure

--- destroy2.sh
+++ destroy2.sh
@@ -2,32 +2,38 @@
 
 #DESTROY DB INSTANCE
 cd rds
+terraform init
 terraform destroy "-var-file=variableValues.tfvars" --auto-approve
 
 #DESTROY HTTPD SERVERS
 cd ../
 cd servers
 cd httpd_server
+terraform init
 terraform destroy "-var-file=variableValues.tfvars" --auto-approve
 
 #DESTROY TOMCAT
 cd ../
 cd tomcat_server
+terraform init
 terraform destroy "-var-file=variableValues.tfvars" --auto-approve
 
 #DESTROY ELB 
 cd ../../
 cd elb
+terraform init
 terraform destroy "-var-file=variableValues.tfvars" --auto-approve
 
 #DESTROY AUTOSCALING
 cd ..
 cd asg_module
+terraform init
 terraform destroy "-var-file=variableValues.tfvars" --auto-approve
 
 #DESTROY CLOUDFRONT
 cd ../
 cd cloudfront
+terraform init
 terraform destroy "-var-file=variableValues.tfvars" --auto-approve
 
 #DESTROY VPC
